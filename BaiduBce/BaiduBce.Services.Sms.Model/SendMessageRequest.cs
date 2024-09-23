f58ff44a387e774916fa884759ecb40cf65d0fa5
MouriNaruto <Kenji.Mouri@outlook.com>
2024-09-23 19:29:39 +0800

    Add BaiduBde project.

---
+++ BaiduBce/BaiduBce.Services.Sms.Model/SendMessageRequest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+using BaiduBce.Model;
+
+namespace BaiduBce.Services.Sms.Model;
+
+public class SendMessageRequest : BceRequestBase
+{
+	public string Mobile { get; set; }
+
+	public string Template { get; set; }
+
+	public string SignatureId { get; set; }
+
+	public Dictionary<string, string> ContentVar { get; set; }
+
+	public string Custom { get; set; }
+
+	public string UserExtId { get; set; }
+
+	[JsonPropertyName("merchantUrlId")]
+	public string CallbackUrlId { get; set; }
+
+	public string ClientToken { get; set; }
+}
---
+++ BaiduBce/BaiduBce.Util/LogUtils.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace BaiduBce.Util
+{
+    public class LogUtils
+    {
+        public static ILoggerFactory Factory = LoggerFactory.Create(
+            Builder =>
+            {
+                Builder.AddConsole();
+                Builder.AddEventLog();
+                Builder.SetMinimumLevel(LogLevel.Trace);
+            });
+    }
+}
